1d3080912ed82b486bbf4ebc649e42c1f739e075
PongthepGeo <pongtep_tong@hotmail.com>
2022-09-05 23:01:50 +0700

    Sep 05

--- 13_image2csv_part_1.py
+++ 13_image2csv_part_1.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('./Libs') 
-import functions as cf
+import functions as f
 #-----------------------------------------------------------------------------------------#
 import pandas as pd
 import geopandas as gpd
@@ -8,6 +8,7 @@ import rasterio
 from rasterio.features import rasterize
 import matplotlib.pyplot as plt
 import numpy as np
+#-----------------------------------------------------------------------------------------#
 
 '''
 Part 1: This exercise demonstrates a simple data transformation from satellite imagery into tabular data. One tile contains 1317 farm ids and each id indicates only a single crop type.
@@ -19,6 +20,7 @@ step 1: create dataframe
 
 # NOTE read label from csv
 df_1 = pd.read_csv('save_tabular/shape_file.csv')
+# print(df_1)
 # NOTE allocate dictionary
 data = {
         'crop_types': [],
@@ -26,7 +28,9 @@ data = {
 		'B2_median' : []
         }
 df = pd.DataFrame(data)
+# NOTE one column
 df['crop_types'] = df_1['crop_type']
+# print(df)
 
 '''
 step 2: extract farm ids (small images)
@@ -35,17 +39,20 @@ step 2: extract farm ids (small images)
 # NOTE import satellite image and shape file
 file_name = '../datasets/sentinel_2/2020/20200107/IMG_DATA/47PQS_20200107_B02.jp2'
 img_profile = rasterio.open(file_name).profile
-shape_file = gpd.read_file('../datasets/shape_files/traindata.shp').to_crs(img_profile['crs'])
+# shape_file = gpd.read_file('../datasets/shape_files/traindata.shp').to_crs(img_profile['crs'])
+shape_file = gpd.read_file('../datasets/shape_files/traindata.shp')
 image = rasterio.open(file_name)
 image = image.read(); image = image[0, :, :]
 image[np.isnan(image)] = 0 # replace nan with zeros (numpy.ndarray)
 # NOTE demo --> extract 1 farm id (10)
-farm = rasterize(shapes=(shape_file.geometry[220], int(shape_file.crop_type[220])), 
-						out_shape=(img_profile['width'], img_profile['height']),
-						transform=img_profile['transform'])
-farm_id = cf.extract_farm_id(image, farm, 'yes')
+farm = rasterize(shapes=(shape_file.geometry[220],
+                 int(shape_file.crop_type[220])), 
+				 out_shape=(img_profile['width'],
+                 img_profile['height']),
+				 transform=img_profile['transform'])
+farm_id = f.extract_farm_id(image, farm, 'yes')
 plt.imshow(farm_id)
-plt.show()
+# plt.show()
 
 '''
 step 3: calculate median and mean and store in dictionary
