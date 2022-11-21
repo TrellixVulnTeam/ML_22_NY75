ee68fd2654a04f8de85a7aa776d4a30ba9a92b87
PongthepGeo <pongtep_tong@hotmail.com>
2022-11-21 23:20:17 +0700

    Nov 21

--- 28_optuna_prune.py
+++ 28_optuna_prune.py
@@ -5,7 +5,7 @@ import numpy as np
 import xgboost as xgb
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, f1_score
 #------------------------------------------------------------------------------------------#
 import warnings
 warnings.filterwarnings('ignore')
@@ -35,29 +35,42 @@ def objective(trial, data=X, target=y):
                                                         stratify=y)
     param = {
         'tree_method':'gpu_hist',  
+        'booster': 'gbtree',
         'lambda': trial.suggest_loguniform('lambda', 1e-3, 10.0),
         'alpha': trial.suggest_loguniform('alpha', 1e-3, 10.0),
-        'colsample_bytree': trial.suggest_categorical('colsample_bytree', [0.3, 0.4, 0.5, 0.6, 0.7,0.8, 0.9, 1.0]),
-        'subsample': trial.suggest_categorical('subsample', [0.4, 0.5, 0.6, 0.7, 0.8, 1.0]),
-        'learning_rate': trial.suggest_categorical('learning_rate', [0.008, 0.01, 0.012, 0.014, 0.016, 0.018, 0.02]),
-        # 'n_estimators': 10000,
-        'max_depth': trial.suggest_categorical('max_depth', [5, 7, 9, 11, 13, 15, 17]),
+        'colsample_bytree': trial.suggest_float('colsample_bytree', 1e-1, 1.),
+        'subsample': trial.suggest_float('subsample', 1e-1, 1.),
+        'learning_rate': trial.suggest_float('learning_rate', 1e-5, 1.),
+        'eta': trial.suggest_float('eta', 1e-5, 1.),
+        'n_estimators': trial.suggest_categorical('n_estimators', [1, 10, 100, 10000]),
+        'max_depth': trial.suggest_int('max_depth', 3, 20),
+        'max_leaves': trial.suggest_int('max_leaves', 3, 20),
         'min_child_weight': trial.suggest_int('min_child_weight', 1, 300)
     }
-    model = xgb.XGBClassifier(**param, gpu_id=1)  
+    model = xgb.XGBClassifier(**param,
+                              num_class=4,
+                              eval_metric='mlogloss', # cost function
+                              gpu_id=1)  
     model.fit(train_x, train_y, eval_set=[(test_x, test_y)], early_stopping_rounds=100,verbose=False)
     preds = model.predict(test_x)
-    rmse = mean_squared_error(test_y, preds, squared=False)
-    return rmse
+    # rmse = mean_squared_error(test_y, preds, squared=False) # evaluation matric
+    weigthed_f1_score = f1_score(test_y, preds, average='weighted') # evaluation matric
+    return weigthed_f1_score
 
 '''
 step 3: begin tuning parameters
 '''
 
-study = optuna.create_study(direction='minimize')
-study.optimize(objective, n_trials=10)
+n_train_iter = 500
+
+sampler = optuna.samplers.TPESampler(seed=42) 
+# pruner  = optuna.pruners.SuccessiveHalvingPruner()
+pruner  = optuna.pruners.HyperbandPruner(min_resource=1, max_resource=n_train_iter, reduction_factor=3)
+study   = optuna.create_study(sampler=sampler, pruner=pruner, direction='minimize')
+study.optimize(objective, n_trials=n_train_iter, gc_after_trial=True)
 print('Number of finished trials:', len(study.trials))
 print('Best trial:', study.best_trial.params)
+
 
 '''
 step 4: plot results 
@@ -74,10 +87,12 @@ fig = optuna.visualization.plot_slice(study)
 fig.show()
 # NOTE plot_contour: plots parameter interactions on an interactive chart. You can choose which hyperparameters you would like to explore.
 fig = optuna.visualization.plot_contour(study, params=['alpha',
-                            'min_child_weight',
-                            'subsample',
-                            'learning_rate',
-                            'subsample'])
+                                                       'n_estimators', 
+                                                       'min_child_weight', 
+                                                       'subsample', 
+                                                       'learning_rate', 
+                                                       'subsample'
+                                                      ])
 fig.show()
 # NOTE plot parameter imprtances
 fig = optuna.visualization.plot_param_importances(study)
